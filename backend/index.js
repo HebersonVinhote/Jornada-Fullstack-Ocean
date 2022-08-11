47da3ee1656cef7b14cbc194473592799deadd90
Heberson_Vinhote <97611591+HebersonVinhote@users.noreply.github.com>
2022-08-11 10:12:16 -0400

    Criando Lista de Objetos

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,36 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+  res.send('Hello World');
+});
+
+app.get('/oi', function (req, res) {
+  res.send('Heberson Vinhote');
+});
+
+
+//o back end armazena as pontuações das jogadas
+// criar a lista com a pontuação
+
+const lista = [
+  {
+    id: 1,
+    nome: "Heberson",
+    pontos: 90
+  },
+  
+  {
+    id: 2,
+    nome: "Fabiana",
+    pontos: 45
+  },
+  {
+    id: 3,
+    nome: "Loki",
+    pontos: 47
+  },
+  
+];
+
 
 app.listen(3000)
