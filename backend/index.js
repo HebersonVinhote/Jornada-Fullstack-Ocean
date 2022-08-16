f371e5923aa4f39c83f3213eb78a6dc91c647af8
Heberson_Vinhote <97611591+HebersonVinhote@users.noreply.github.com>
2022-08-16 10:39:31 -0400

    conexão com o banco de dados

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,22 @@
 const express = require('express')
+const { MongoClient } = require('mongodb')
+
+const url = "mongodb://localhost:27017"
+const dbname = "jornada-fullstack-ocean"
+
+async function main() {
+
+console.log("conectando ao banco de dados")
+const client = await MongoClient.connect(url);
+const db = client.db(dbname);
+const collection = db.collection("pontuacoes")
+
+console.log("Banco de dados conectados")
 const app = express()
+
+//Sinalizando para o express que estamos usando JSON no body das requisições
+
+app.use(express.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World');
@@ -35,17 +52,29 @@ const lista = [
 
 //endpoint READ ALL - [GET] /pontuacoes
 
-app.get("/pontuacoes", function (req, res) {
-  res.send(lista);
+app.get("/pontuacoes", async function (req, res) {
+  const itens = await collection.find().toArray();
+  res.send(itens);
 });
 
 //endpoint de CREATE - [POST] /pontuacoes
 
 app.post("/pontuacoes", function (req, res) {
   const item = req.body;
-  console.log(item);
   
-  res.send("Criar uma pontuação");
+  // console.log(item);
+
+  //Adicionando o item na lista
+
+  lista.push({
+    id: lista.length + 1,
+    nome:item.nome,
+    pontos: item.pontos,
+  })
+  res.send("Item criado com sucesso");
 })
 
 app.listen(3000)
+}
+
+main();
