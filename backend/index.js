b0c25d750543ecf90a2d04c995a667984b74ad3d
Heberson_Vinhote <97611591+HebersonVinhote@users.noreply.github.com>
2022-08-16 11:04:32 -0400

    ordenação de lista e limite de exibição

--- backend/index.js
+++ backend/index.js
@@ -31,47 +31,32 @@ app.get('/oi', function (req, res) {
 // criar a lista com a pontuação
 
 const lista = [
-  {
-    id: 1,
-    nome: "Heberson",
-    pontos: 90
-  },
-  
-  {
-    id: 2,
-    nome: "Fabiana",
-    pontos: 45
-  },
-  {
-    id: 3,
-    nome: "Loki",
-    pontos: 47
-  },
-  
 ];
 
 //endpoint READ ALL - [GET] /pontuacoes
 
 app.get("/pontuacoes", async function (req, res) {
-  const itens = await collection.find().toArray();
+  const itens = await collection.find().sort({ pontos: -1 }).limit(10).toArray();
+  
   res.send(itens);
 });
 
 //endpoint de CREATE - [POST] /pontuacoes
 
-app.post("/pontuacoes", function (req, res) {
+app.post("/pontuacoes", async function (req, res) {
   const item = req.body;
   
   // console.log(item);
 
   //Adicionando o item na lista
 
-  lista.push({
-    id: lista.length + 1,
-    nome:item.nome,
-    pontos: item.pontos,
-  })
-  res.send("Item criado com sucesso");
+//  lista.push({
+//    id: lista.length + 1,
+//    nome:item.nome,
+//    pontos: item.pontos,
+//  })
+  await collection.insertOne(item);
+  res.send(item);
 })
 
 app.listen(3000)
